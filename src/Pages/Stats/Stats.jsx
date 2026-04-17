6c5add841ad8ebe9efff888e6aeee85b2fc7427b
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-04-17 21:46:14 +0600

    added stats

--- src/Pages/Stats/Stats.jsx
+++ src/Pages/Stats/Stats.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
+import "../../App.css";
+import { Legend, Pie, PieChart, Tooltip } from "recharts";
+import { HistoryContext } from "../../Context/HistoryProvider";
 
 const Stats = () => {
-  return <div>Stats</div>;
+  const { call } = useContext(HistoryContext);
+  const callCount = call.filter((item) => item.type == "Call").length;
+  const chatCount = call.filter((item) => item.type == "Chat").length;
+  const videoCount = call.filter((item) => item.type == "Video").length;
+  const data = [
+    { name: "Text", value: chatCount, fill: "#7E35E1" },
+    { name: "Call", value: callCount, fill: "#244D3F" },
+    { name: "Video", value: videoCount, fill: "#37A163" },
+  ];
+  return (
+    <div className="my-9 px-4 md:px-0 container mx-auto bg-[#F8FAFC]">
+      {/*Stats intro */}
+
+      <h1 className="text-[#1F2937] font-bold text-3xl">Friendship Analytics </h1>
+
+      {/* History */}
+      <div className="shadow-lg rounded-2xl bg-white my-4 p-5">
+        <p className="primary-color text-[1.1rem] font-semibold">By Interaction Type</p>
+
+        <div className="flex items-center justify-center pt-4 pb-10">
+          <PieChart
+            style={{ width: "100%", maxWidth: "300px", maxHeight: "40vh", aspectRatio: 1 }}
+            responsive
+          >
+            <Pie
+              data={data}
+              innerRadius="80%"
+              outerRadius="100%"
+              // Corner radius is the rounded edge of each pie slice
+              cornerRadius="50%"
+              fill="#8884d8"
+              // padding angle is the gap between each pie slice
+              paddingAngle={5}
+              dataKey="value"
+              isAnimationActive={true}
+            />
+            <Tooltip></Tooltip>
+            <Legend iconType="circle" className="mt-6"></Legend>
+          </PieChart>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Stats;
